0493b6648ee5ace0c3fe7eb1326bffda3f9ba33a
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-03-04 08:58:46 -0400

    Complete SumOfPositive in python

--- SumOfPositive.py
+++ SumOfPositive.py
@@ -1,12 +1,6 @@
 # Codewars
 # You get an array of numbers, return the sum of all of the positives ones.
 
-# My psuedocode
-# create a variable to store the sum
-# loop through the array
-#   add the current element to the sum
-# return the sum
-
 def positive_sum(arr):
     result = 0
     for i in range(len(arr)):
