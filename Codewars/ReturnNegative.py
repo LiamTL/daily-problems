b07776a28a62b3af5f09c60dd85505791b4dead0
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-03-07 09:30:03 -0400

    Complete ReturnNegative in python

---
+++ Codewars/ReturnNegative.py
@@ -0,0 +1,6 @@
+# In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+def make_negative( number ):
+    if number > 0:
+        return number * -1
+    return number
