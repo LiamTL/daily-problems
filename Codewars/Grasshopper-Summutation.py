d1a29c1c8091a05f0e27a73f5d258927e9d53293
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-03-09 09:29:30 -0400

    Complete Grasshopper-Summutation in python

---
+++ Codewars/Grasshopper-Summutation.py
@@ -0,0 +1,6 @@
+# Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+def summation(num):
+    if num > 1:
+        return num + summation(num - 1)
+    return 1
