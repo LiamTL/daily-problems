9775ca857b7831144d9fefec16823adcca91956c
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-03-25 09:36:23 -0400

    Refactor You Only Need One in python

--- Codewars/YouOnlyNeedOne.py
+++ Codewars/YouOnlyNeedOne.py
@@ -1,7 +1,4 @@
 # You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
 # Return true if the array contains the value, false if not.
 def check(seq, elem):
-    for x in seq:
-        if x == elem:
-            return True
-    return False
+    return elem in seq
