a6ac78e31d9fa1a30e3b75fff63ea0b432a463be
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-03-26 09:22:51 -0400

    Complete AnotherCalculateAverageSolution in python

---
+++ Codewars/AnotherCalculateAverage.py
@@ -0,0 +1,6 @@
+# If you "like" one-liners this is your guy
+# Explanation: (For Beginners)
+# Return the sum of the list's items divided by the length of the list
+
+def find_average(numbers):
+    return sum(numbers) / len(numbers)
