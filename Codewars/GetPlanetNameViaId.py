009d32f513a095da6582f8434d9b2583b5978075
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-04-01 16:41:41 -0400

    Refactor GetPlanetNameViaId in python

--- Codewars/GetPlanetNameViaId.py
+++ Codewars/GetPlanetNameViaId.py
@@ -1,14 +1,12 @@
 def get_planet_name(id):
-    if id == 1:return "Mercury"
-    
-    elif id == 2:return "Venus"
-
-    elif id == 3:return "Earth"
-    
-    elif id == 4:return "Mars"
-
-    elif id == 5:return "Jupiter"
-    
-    elif id == 6:return "Uranus"
-
-    else:return "Neptune"
+    planets = {
+        1: "Mercury",
+        2: "Venus",
+        3: "Earth",
+        4: "Mars",
+        5: "Jupiter",
+        6: "Saturn",
+        7: "Uranus", 
+        8: "Neptune",
+    }
+    return planets[id]
