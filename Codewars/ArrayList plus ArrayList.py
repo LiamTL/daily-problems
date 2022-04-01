dae694616af0521937223fdc7672cb319e7e9524
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-04-01 16:33:07 -0400

    Complete ArrayListPlusArrayList in python

---
+++ Codewars/ArrayList plus ArrayList.py
@@ -0,0 +1,5 @@
+# I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
+# I'll appreciate for your help.
+
+def array_list_sum(arr1, arr2):
+    return sum(arr1) + sum(arr2)
