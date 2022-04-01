2cb5517aefa40d4bf7fc1772daef41de99bf1bf7
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-04-01 16:40:01 -0400

    Complete GetPlanetNameViaId in python

---
+++ Codewars/GetPlanetNameViaId.py
@@ -0,0 +1,14 @@
+def get_planet_name(id):
+    if id == 1:return "Mercury"
+    
+    elif id == 2:return "Venus"
+
+    elif id == 3:return "Earth"
+    
+    elif id == 4:return "Mars"
+
+    elif id == 5:return "Jupiter"
+    
+    elif id == 6:return "Uranus"
+
+    else:return "Neptune"
