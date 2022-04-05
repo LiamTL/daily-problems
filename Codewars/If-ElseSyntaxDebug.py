88b6ff765ac15cd8d97ae96dbc8843d9b332bc87
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-04-05 08:20:07 -0400

    Complete If-ElseSyntaxDebug in python

---
+++ Codewars/If-ElseSyntaxDebug.py
@@ -0,0 +1,8 @@
+# While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. 
+# Unfortunately, Greg made some errors while creating the function.
+
+def check_alive(health):
+    if health <= 0:
+        return False
+    
+    return True
