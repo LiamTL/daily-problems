8082aeaeb0d81214a07b8c39201480d757166807
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-04-06 09:14:38 -0400

    Complete Two-fer in python

---
+++ Exercism/Two-fer.py
@@ -0,0 +1,11 @@
+# Two-fer or 2-fer is short for two for one. One for you and one for me.
+# Given a name, return a string with the message:
+# One for name, one for me.
+# Where "name" is the given name.
+# However, if the name is missing, return the string:
+# One for you, one for me.
+
+
+def two_fer(name = "you"):
+    return f"One for {name}, one for me."
+    
