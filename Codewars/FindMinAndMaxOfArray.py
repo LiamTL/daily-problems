4788ae44486b79ed9856ed02870e1687db446c6e
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-04-20 11:30:39 -0400

    Complete FindMinAndMaxOfArray in python

---
+++ Codewars/FindMinAndMaxOfArray.py
@@ -0,0 +1,10 @@
+# Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language).
+# That receive a list of integers as input and return, respectively.
+# The largest and lowest number in that list.
+
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)
+
