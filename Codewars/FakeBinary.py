a3e84dc8d4b4bbba3b6cffbb205c0069a62621c7
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-04-25 08:55:54 -0400

    Complete FakeBinary in python

---
+++ Codewars/FakeBinary.py
@@ -0,0 +1,8 @@
+def fake_bin(x):
+    result = ""
+    for num in x:
+        if int(num) < 5:
+            result = result + "0"
+        else:
+            result = result + "1"
+    return result
