73efe9c2aef20bb668514076704d96257b879320
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-04-28 09:19:10 -0400

    Add Description to StringCleaning in python

--- Codewars/StringCleaning.py
+++ Codewars/StringCleaning.py
@@ -1,3 +1,6 @@
+# Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+# Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. 
+
 def string_clean(s):
     cleaned_str = ""
     for item in s:
