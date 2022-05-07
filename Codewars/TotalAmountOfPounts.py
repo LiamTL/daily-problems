e2ea0c317af633a9ce931deb532801c405b5bc8f
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-05-07 08:15:41 -0400

    Complete TotalAmountOfPounts in python

---
+++ Codewars/TotalAmountOfPounts.py
@@ -0,0 +1,11 @@
+# Write a function that takes such collection and counts the points of our team in the championship.
+
+def points(games):
+    count = 0
+    for score in games:
+        res = score.split(':')
+        if res[0]>res[1]:
+            count += 3
+        elif res[0] == res[1]:
+            count += 1
+    return count
