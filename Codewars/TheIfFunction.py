e352f62d3ab6181c6f6078b9d2424c351b386b8b
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-05-12 07:49:12 -0400

    Refactor TheIfFunction in python

--- Codewars/TheIfFunction.py
+++ Codewars/TheIfFunction.py
@@ -1,7 +1,7 @@
 # Create a function called _if which takes 3 arguments: a boolean value bool and 2 functions (which do not take any parameters): func1 and func2
 
 def _if(bool, func1, func2):
-    if bool == True:
+    if bool:
         func1()
     else:
         func2()
