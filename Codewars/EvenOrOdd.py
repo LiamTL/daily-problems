49bf15a28984bb632e6a60256576a5d7d5a97eb1
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-05-24 11:39:29 -0400

    Refactor EvenOrOdd in python

--- Codewars/EvenOrOdd.py
+++ Codewars/EvenOrOdd.py
@@ -1,7 +1,4 @@
 # Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 def even_or_odd(number):
-    if number % 2 == 0:
-        return "Even"
-    else:
-        return "Odd"
+    return "Even" if number % 2 == 0 else "Odd"
