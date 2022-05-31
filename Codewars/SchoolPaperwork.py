1456cb780f64e875928efa9550d94610ae5cb006
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-05-31 08:06:46 -0400

    Refactor SchoolPaperwork in python

--- Codewars/SchoolPaperwork.py
+++ Codewars/SchoolPaperwork.py
@@ -2,6 +2,4 @@
 # You know that there are 'n' classmates and the paperwork has 'm' pages.
 
 def paperwork(n, m):
-    if m <= 0 or n <= 0:
-        return 0
-    return n * m
+    return 0 if n <= 0 or m <= 0 else n * m
