104012b54d4fba51154e10adbb46eefe4733e025
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-06-25 11:34:53 -0400

    Refactor DoubleChar in python

--- Codewars/DoubleChar.py
+++ Codewars/DoubleChar.py
@@ -1,7 +1,5 @@
 def double_char(string):
     doubled = ""
     for letter in range(len(string)):
-        doubled += string[letter]
-        doubled += string[letter]        
-        
+        doubled += string[letter] * 2
     return doubled
