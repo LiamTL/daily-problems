61d631319392cf4f7addca5667354914e72a3032
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-07-14 19:14:35 -0400

    Refactor YourASquare and Update YouCan'tCodeUnderPressure in python

--- Codewars/YouCan'tCodeUnderPressure.py
+++ Codewars/YouCan'tCodeUnderPressure.py
@@ -1,4 +1,2 @@
-# Completed in 3.79 seconds!
-
 def double_integer(i):
     return i * 2
--- Codewars/YourASquare!.py
+++ Codewars/YourASquare!.py
@@ -1,5 +1,4 @@
 # Given an integral number, determine if it's a square number
-import math
 
-def is_square(n):
-    return n > -1 and math.sqrt(n) % 1 == 0;
+def is_square(n):    
+    return n >= 0 and (n**0.5) % 1 == 0
