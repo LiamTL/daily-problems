0c5e0d42866e3a1325f28e330499328cda944071
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-07-17 11:22:25 -0400

    Update AbbreviateTwoWordNameUpdate in python

--- Codewars/AbbreviateTwoWordName.py
+++ Codewars/AbbreviateTwoWordName.py
@@ -1,9 +1,10 @@
+# Convert name to uppercase
+# Split string
+# Get first letters
+# Return + join letters with a seperating "."
+
 def abbrev_name(name):
-    # Convert name to uppercase
     uppername = name.upper()
-    # Split string
     words = uppername.split()
-    # Get first letters
     letters = [word[0] for word in words]
-    # Return + join letters with a seperating "."
     return (".".join(letters))
