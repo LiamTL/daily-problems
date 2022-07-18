942255b86ce14c3578986a34af76ce0589236770
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-07-18 09:03:53 -0400

    Refactor VowelCount in python

--- Codewars/VowelCount.py
+++ Codewars/VowelCount.py
@@ -1,8 +1,11 @@
 # Return the number (count) of vowels in the given string.
 
 def get_count(sentence):
-    vow_count = 0
-    for vow in sentence:
-        if vow == "a" or vow == "e" or vow == "i" or vow == "o" or vow == "u":
-            vow_count += 1
-    return vow_count
+    valid_vows = "aeiouAEIOU"
+    vows_found = 0
+    for poss_vow in sentence:
+        if poss_vow in valid_vows:
+            vows_found += 1
+            
+    return vows_found
+        
