ba2c3105e22d2b9454e76dad2ca02cf42873666a
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-08-17 11:49:04 -0400

    Refactor DisemvowelTrolls in python

--- Codewars/DisemvowelTrolls.py
+++ Codewars/DisemvowelTrolls.py
@@ -1,4 +1,8 @@
-def disemvowel(s):
-    for i in "aeiouAEIOU":
-        s = s.replace(i,'')
-    return s
+def disemvowel(string):
+    vowels = "aeiouAEIOU"
+    fixxed = ""
+    for vow_idx in range(len(string)):
+        if string[vow_idx] not in vowels:
+            fixxed += string[vow_idx]
+            
+    return fixxed
