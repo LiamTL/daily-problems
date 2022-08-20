a8701a468d1d025a4fcb3445bd840c241215f93a
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-08-20 13:51:40 -0400

    Refactor SumMixedArray in python

--- Codewars/SumMixedArray.py
+++ Codewars/SumMixedArray.py
@@ -1,8 +1,8 @@
-def sum_mix(arr_of_strng_and_ints):
-    result = 0
-    for val in arr_of_strng_and_ints:
-        try:
-            result += val
-        except TypeError:
-            result += int(val)
-    return result
+def sum_mix(arr):
+    sumz = 0
+    for num in range(len(arr)):
+        if arr[num] is int():
+            sumz += arr[num]
+        else:
+            sumz += int(arr[num])      
+    return sumz
