afb6645aadf484743e588e45e61be6fdbbfed6e7
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-08-25 09:53:58 -0400

    Refactor IsTheStringUppercase in python

--- Codewars/IsTheStringUppercase.py
+++ Codewars/IsTheStringUppercase.py
@@ -1,4 +1,3 @@
 # Create a method to see whether the string is ALL CAPS (Excluding any lowercase letters).
 
-def is_uppercase(inp):
-    return True if inp == inp.upper() else False
+is_uppercase = lambda inp: True if inp == inp.upper() else False
