1e4d6d9773a46146a2413eaa1cb1d3f5f822f27b
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-07 17:59:22 -0400

    Complete KeepUpTheHoop in javascript

---
+++ Codewars/KeepUpTheHoop.js
@@ -0,0 +1,15 @@
+// Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+// Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+function hoopCount(n){
+    if (n >= 10){
+        return "Great, now move on to tricks"
+    }
+    else {
+        return "Keep at it until you get it"
+    }
+}
+
+console.log(hoopCount(3))
+console.log(hoopCount(10))
+console.log(hoopCount(11))
