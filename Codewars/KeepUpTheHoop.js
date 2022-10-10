072da58bc2554fc344de5005d11bc8c5bc1f4241
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-10 09:00:26 -0400

    Update KeepUpTheHoop in javascript

--- Codewars/KeepUpTheHoop.js
+++ Codewars/KeepUpTheHoop.js
@@ -4,12 +4,7 @@
 function hoopCount(n){
     if (n >= 10){
         return "Great, now move on to tricks"
-    }
-    else {
+    } else {
         return "Keep at it until you get it"
     }
 }
-
-console.log(hoopCount(3))
-console.log(hoopCount(10))
-console.log(hoopCount(11))
