8e91c154fad38ce4fc8010b7acf0d0d039749f36
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-14 08:15:26 -0400

    Complete ParseNiceIntIntoChar in javascript

---
+++ Codewars/ParseNiceIntIntoChar.js
@@ -0,0 +1,5 @@
+// Write a program that returns the girl's age (0-9) as an integer.
+// Assume the test input string is always a valid string.
+function getAge(inputString){
+    return parseInt(inputString.slice(0, 1))
+  }
