98cd92acb2159b1c5eccd1beb05063e0e509029d
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-17 09:08:42 -0400

    Complete FindMultiplesOfANumber in python

--- Codewars/FindMultiplesOfANumber.py
+++ Codewars/FindMultiplesOfANumber.py
@@ -1,3 +1,7 @@
+# In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit.
+# If limit is a multiple of integer, it should be included as well.
+# For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
 def find_multiples(integer, limit):
     multiples = []
     for multiple in range(integer, limit + 1):
