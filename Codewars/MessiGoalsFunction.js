46d9f2dbce40fca41f5bcab891dd2b24388a2ee0
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-18 09:02:30 -0400

    Complete MessiGoalsFunction in javascript

---
+++ Codewars/MessiGoalsFunction.js
@@ -0,0 +1,6 @@
+// Messi is a soccer player with goals in three leagues.
+// Complete the function to return his total number of goals in all three leagues.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
