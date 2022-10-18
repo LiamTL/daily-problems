4241d2a5b9baa26dbad9a960b9487a7775e1ae12
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-18 08:59:16 -0400

    Complete MessiGoalsFunction in python

---
+++ Codewars/MessiGoalsFunction.py
@@ -0,0 +1,5 @@
+# Messi is a soccer player with goals in three leagues.
+# Complete the function to return his total number of goals in all three leagues.
+
+def goals(laLiga, copaDelRey, championsLeague):
+    return laLiga + copaDelRey + championsLeague
