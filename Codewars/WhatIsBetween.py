768d1dc5b3a4ec202a4c21c7fdc9217fe49df427
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-20 08:55:22 -0400

    Complete WhatIsBetween in python

--- Codewars/WhatIsBetween.py
+++ Codewars/WhatIsBetween.py
@@ -1,1 +1,4 @@
+# Complete the function that takes two integers (a, b, where a < b).
+# Return an array of all integers between the input parameters, a and b.
+
 between = lambda a, b: list(range(a, b + 1))
