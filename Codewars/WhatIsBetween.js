311d787ab8819c2cb5dcf417f7e53a558b156463
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-20 09:11:24 -0400

    Complete WhatIsBetween in javascript

--- Codewars/WhatIsBetween.js
+++ Codewars/WhatIsBetween.js
@@ -1,3 +1,6 @@
+// Complete the function that takes two integers (a, b, where a < b).
+// Return an array of all integers between the input parameters, a and b.
+
 function between(a, b) {
     lst = []
     for (let i = a; i <= b; i++) {
