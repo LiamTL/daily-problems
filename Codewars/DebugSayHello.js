8bd98d6f9e6934002ceafdc129c67a59395c3fd9
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-24 10:28:51 -0400

    Refactor DebugSayHello in javascript

--- Codewars/DebugSayHello.js
+++ Codewars/DebugSayHello.js
@@ -1,2 +1,2 @@
 // The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. 
-const sayHello = (name) => {return "Hello, " + name}
+const sayHello = (name) => "Hello, " + name
