c91d6dd0d542fe5f92749126c4e831af8c7dabbd
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-25 09:39:06 -0400

    Complete CapitalizationAndMutablity in javascript

---
+++ Codewars/CapitalizationAndMutablity.js
@@ -0,0 +1,4 @@
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+// Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended.
+
+const capitalizeWord = (x) => { return x[0].toUpperCase() + x.slice(1, x.length)}
