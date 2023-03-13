84cbb2eaacd9f95752a3bb65c3a39a8d6b60b316
LiamTL <93681753+LiamTL@users.noreply.github.com>
2023-03-13 13:36:17 -0400

    Refactor ReturnNegative in python

--- Codewars/ReturnNegative.py
+++ Codewars/ReturnNegative.py
@@ -1,6 +1,5 @@
 # In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+# Sorry for being gone this long. Life's been a little rough around the edges but now I -hopefully- have the time to do more coding problems
+# So for now I'll see if I can do more coding (Been gone sooooo long skills gon' need some MEGA WD-40)
 
-def make_negative( number ):
-    if number > 0:
-        return number * -1
-    return number
+make_negative = lambda num: num * -1 if num >= 0 else num
